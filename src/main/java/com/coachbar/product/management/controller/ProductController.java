0595ccfc5ba08c9e1e17c0df332cdc10b26dc6dd
yash shukla <yashshukla1319@gmail.com>
2024-11-16 15:47:05 +0530

    fix: add API document

--- src/main/java/com/coachbar/product/management/controller/ProductController.java
+++ src/main/java/com/coachbar/product/management/controller/ProductController.java
@@ -3,9 +3,9 @@ package com.coachbar.product.management.controller;
 import com.coachbar.product.management.exceptions.ResourceNotFoundException;
 import com.coachbar.product.management.model.entity.Product;
 import com.coachbar.product.management.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -25,11 +25,13 @@ public class ProductController {
         this.productService = productService;
     }
 
+    @Operation(summary = "Get all products", description = "Get all products")
     @GetMapping
     public ResponseEntity<List<Product>> getProducts() {
         return ResponseEntity.ok(productService.getProducts());
     }
 
+    @Operation(summary = "Get product by ID", description = "Get product by ID")
     @GetMapping("/{id}")
     public ResponseEntity<Product> getProductById(@PathVariable Long id) {
         Optional<Product> product = productService.getProductById(id);
@@ -39,6 +41,7 @@ public class ProductController {
                 .orElseThrow(() -> new ResourceNotFoundException("Product with Id does not exists."));
     }
 
+    @Operation(summary = "Create a new product", description = "Add a new product to the inventory")
     @PostMapping
     public ResponseEntity<Product> createProduct(@Valid @RequestBody Product product) {
         Product savedProduct = productService.createProduct(product);
@@ -51,11 +54,13 @@ public class ProductController {
         return ResponseEntity.created(location).body(savedProduct);
     }
 
+    @Operation(summary = "Update a existing product", description = "Update a existing product")
     @PutMapping("/{id}")
     public ResponseEntity<Product> updateProduct(@PathVariable Long id, @Valid @RequestBody Product productDetails) {
         return ResponseEntity.ok(productService.updateProduct(id, productDetails));
     }
 
+    @Operation(summary = "Delete a product by ID", description = "Delete a product by ID")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
         productService.deleteProduct(id);
